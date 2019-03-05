70f1c16fc260e803a90d58ec5639018b4461b102
DianeLam <diane.lamant.simplon@gmail.com>
2019-03-05 17:31:49 +0100

    animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,11 +23,52 @@ function setLanguage(language) {
 }
 setLanguage('fr');
 
-// A faire :
-
-// Fermer menu après clic
-// Background page
+// Progress bar 
 
 function animProgBar() {
     document.getElementsByClassName("progHTML").style.width = '0;';
+}
+
+// Anim icones
+
+let iconesCtc = document.getElementsByClassName("fas");
+
+for (let eachIcones of iconesCtc) {
+
+    eachIcones.addEventListener("mouseenter", function (event) {
+
+        event.target.classList.add("rotate");
+        setTimeout(function () {
+            event.target.classList.remove("rotate");
+        }, 800);
+    })
+}
+
+let iconesCpt = document.getElementsByClassName("fab");
+
+for (let eachIcones of iconesCpt) {
+
+    eachIcones.addEventListener("mouseenter", function (event) {
+
+        event.target.classList.add("rotate");
+        setTimeout(function () {
+            event.target.classList.remove("rotate");
+        }, 800);
+    })
+}
+
+let textCpt = document.getElementsByClassName("compText");
+
+for (let eachTxt of textCpt) {
+
+    eachTxt.addEventListener("mouseenter", function (event) {
+
+        console.log(eachTxt);
+
+
+        event.target.classList.add("pulse");
+        setTimeout(function () {
+            event.target.classList.remove("pulse");
+        }, 1000);
+    })
 }
