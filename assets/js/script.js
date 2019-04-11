82fe9d35246914bee9334fd63d1066a05f4799d2
DianeLam <diane.lamant.simplon@gmail.com>
2019-04-11 17:29:32 +0200

    responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,49 +14,19 @@ imgMenu.forEach((element, index) => {
 });
 
 
+let circleSkill = document.querySelectorAll(".circleSkill");
+let circleActive = document.querySelectorAll(".fa-active");
+let textSkill = document.querySelectorAll(".textSkill");
 
-$('.spiderChart').highcharts({
-
-    chart: {
-        polar: true,
-        type: 'line',
-        style: {
-            fontFamily: "Georgia, serif", // default font
-            fontSize: '12px'
-        }
-    },
-
-    colors: ['#0067ac', '#a02842', '#007680'],
-
-    pane: {
-        size: '80%'
-    },
-
-    xAxis: {
-        categories: ["Time", "Cost Saving", "Productivity", "Reduction in Errors", "ROI", "Revenue Increase", "Payback Period"],
-        tickmarkPlacement: 'on',
-        lineWidth: 0
-    },
-
-    yAxis: {
-        gridLineInterpolation: 'polygon',
-        lineWidth: 0,
-        min: 0,
-        max: 60,
-        tickInterval: 5
-    },
-
-    tooltip: {
-        shared: true,
-        pointFormat: '<span style="color:{series.color}"> <b>{point.y:,.0f}%</b><br/>'
-    },
-
-    legend: {
-        enabled: false
-    },
-
-    series: [{
-        data: [55, 48, 43, 39, 18, 15, 5],
-        pointPlacement: 'on'
-    }]
+circleSkill.forEach((element, index) => {
+    element.addEventListener("mouseover", function () {
+        textSkill[index].style.visibility = "visible";
+        textSkill[index].classList.add("animated", "fadeInDown");
+        circleSkill[index].classList.add("animated", "heartBeat");
+    });
+    element.addEventListener("mouseout", function () {
+        textSkill[index].style.visibility = "hidden";
+        textSkill[index].classList.remove("animated", "fadeInDown");
+        circleSkill[index].classList.remove("animated", "heartBeat");
+    });
 });
