d39dab8dda945961c31bbf83a87fc6f722fc88cc
DianeLam <diane.lamant.simplon@gmail.com>
2019-04-16 16:49:59 +0200

    details

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,27 @@
+// const ScrollReveal = require('scrollreveal');
+// import ScrollReveal from 'scrollreveal';
+// ScrollReveal().reveal('#projects');
+
+
+$('.link_menu').each(function (index, element) {
+    $(element).click(function () {
+        $('html,body').animate({
+            scrollTop: section.offset().top
+        },
+            'slow');
+    });
+});
+
+
+
 let imgMenu = document.querySelectorAll(".img_menu");
 let textMenu = document.querySelectorAll(".text_menu");
 let linkMenu = document.querySelectorAll(".link_menu");
 let toggler = document.querySelector(".toggler");
-console.log(toggler);
 
 toggler.addEventListener("click", function () {
     if (toggler.classList.contains("active")) {
         textMenu.forEach((element, index) => {
-            // textMenu[index].classList.add("slideOutLeft");
             element.style.display = "none";
             textMenu[index].classList.remove("slideInLeft");
             toggler.classList.remove("active");
@@ -18,8 +32,8 @@ toggler.addEventListener("click", function () {
             textMenu[index].classList.add("slideInLeft");
             toggler.classList.add("active");
         });
-    }
-})
+    };
+});
 
 linkMenu.forEach((element, index) => {
     element.addEventListener("click", function () {
@@ -35,7 +49,6 @@ imgMenu.forEach((element, index) => {
     element.addEventListener("mouseover", function () {
         textMenu[index].style.display = "block";
         textMenu[index].classList.add("slideInRight");
-        console.log(textMenu[index]);
 
     });
     element.addEventListener("mouseout", function () {
