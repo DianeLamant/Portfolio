466e7115c1b7586d8321e5711cf725dec6730e38
DianeLam <diane.lamant.simplon@gmail.com>
2019-05-17 14:49:12 +0200

    contact et suppr img

--- erreurs/script.js
+++ erreurs/script.js
@@ -2,6 +2,8 @@ var game = $('#game');
 var width = game.width();
 var height = game.height();
 var mouse = $('#mouse');
+var mouseWidth = mouse.width();
+var mouseHeight = mouse.height();
 
 $(document).ready(function () {
     mouse.click(function () {
@@ -15,8 +17,10 @@ $(document).ready(function () {
 
 mouse.each(function foo() {
 
-    var top = (Math.random() * height) | 0;
-    var left = (Math.random() * width) | 0;
+    var intTop = Math.random();
+    var intLeft = Math.random();
+    var top = ((intTop * height) - mouseWidth) | 0;
+    var left = ((intLeft * width) - mouseHeight) | 0;
 
     $(this).animate({
         left: left,
